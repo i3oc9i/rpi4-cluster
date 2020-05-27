c168fadb710d39291b6bb349ff4e7378b05bb91d
i3oc9i <ivano.coltellacci@gmail.com>
2020-05-27 19:47:10 +0200

    ansible refactoring

--- init.sh
+++ init.sh
@@ -4,5 +4,5 @@ ADMIN_USER=ubuntu
 
 cp ~/.ssh/id_rsa.pub ./files/${ADMIN_USER}_key.pub
 
-ansible-playbook -u ${ADMIN_USER} -k --ask-vault ./init.yaml
+ansible-playbook -u ${ADMIN_USER} -k --ask-vault ./install.yaml
 
